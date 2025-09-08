fcf3bd7f825059851ad6b8c0d3a8606c91c9963f
Daniel008-01 <danban121221@gmail.com>
2025-09-08 22:21:17 +0600

    fix bug

--- src/components/changeNames/ChangeNames.jsx
+++ src/components/changeNames/ChangeNames.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./ChangeNames.scss";
+import "./changeNames.scss";
 
 export default function ChangeNames() {
   const [name1, setName1] = useState("Красный");
